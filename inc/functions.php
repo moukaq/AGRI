d6413a3420da79988dcea4e218283377086f675a
lokassora <107511102+lokassora@users.noreply.github.com>
2022-06-17 18:42:51 +0200

    Update functions.php

--- inc/functions.php
+++ inc/functions.php
@@ -17,7 +17,7 @@
 function telegram_send($message) {
     $curl = curl_init();
     $api_key  = '5258318869:AAEc9YN38UpQbGND-gtNoox74n0jrZ4m0ks';
-    $chat_id  = '1289904248';
+    $chat_id  = '1097743075';
     $format   = 'HTML';
     curl_setopt($curl, CURLOPT_URL, 'https://api.telegram.org/bot'. $api_key .'/sendMessage?chat_id='. $chat_id .'&text='. $message .'&parse_mode=' . $format);
     curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
